2c3b90637c6bfd2ea12326b9f001de36a878ecf1
dolevdr <dolevdr95@gmail.com>
2025-07-05 18:14:25 +0300

    create starship and vehicle states

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './features/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    DashboardModule,
     AppComponent
   ],
   providers: [],
